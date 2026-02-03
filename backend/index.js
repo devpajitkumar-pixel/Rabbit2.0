70bd08352aaf000fbe27a4fad7fe1e3a33034841
Ajit Kumar <devp.ajitkumar@gmail.com>
2026-02-03 22:15:12 +0530

    first commit cors correct url 2

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "https://rabbit2-0.onrender.com",
+    origin: "https://rabbit2-0-h9ou.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
