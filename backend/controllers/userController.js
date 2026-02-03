023cb89dcc36075a5b54c51ed924d535bf0df2e6
Ajit Kumar <devp.ajitkumar@gmail.com>
2026-02-04 02:45:09 +0530

    csrf debug jwt secure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true, // JS can't access
-    secure: false, // true in production (HTTPS)
+    secure: true, // true in production (HTTPS)
     sameSite: "lax", // important for frontend
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
